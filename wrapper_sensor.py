035c0cfaa28e7f99a76eda4d8a8ce028f2490d90
Gabriel Rios <gabriel.rios@nextern.com>
2024-05-02 09:50:56 -0600

    Changed methods and variable names to improve readability of fitting wrapper

--- wrapper_sensor.py
+++ wrapper_sensor.py
@@ -1,10 +1,19 @@
+# CALIBRATION PATH
+
 import numpy as np
 import scipy.io as sci
 
 import sensorfitting as sensorfitting
-loc = r'D:\Sumedh\Projects\Methods for psychiatric DBS programming'
-A = sci.loadmat('gamma.mat')
-SDUall = {'interference_SDU': np.random.randint(2, size=(1, 500)),
-          'yRT_SDU': A['Yn'].T}
-Iter = 250
-sensorfitting.sensorfitting(1, loc, SDUall, Iter)
+
+outputLocation = r'D:\Sumedh\Projects\Methods for psychiatric DBS programming'
+fullGammaDistributionData = sci.loadmat('gamma.mat') # Will come from patient MSIT in prod
+reactionTimesData = fullGammaDistributionData['Yn'].T
+interferenceData = np.random.randint(2, size=(1, 500))
+
+inputData = {'interference_SDU': interferenceData,
+          'yRT_SDU': reactionTimesData}
+iterations = 250
+
+sensorfitting.fitSensor(1, outputLocation, inputData, iterations)
+
+
