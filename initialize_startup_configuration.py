76af14f9cc809d9ef39b6e5bb8025901c692f10c
Sumedh Sopan Nagrale <51673422+SumedhNagrale@users.noreply.github.com>
2024-05-08 14:49:06 -0500

    updating the variable names
    the variable names are as per the discussion between me and Gabriel, to improve readablity

--- initialize_startup_configuration.py
+++ initialize_startup_configuration.py
@@ -1,4 +1,3 @@
-from object_oriented_optimization import StartupConfiguration
 import pickle
 import optimization_toolkit.ts_armselect_blockwise_prior_update as recommender
 import optimization_toolkit.ts_call_observer as sensor_call
@@ -10,29 +9,29 @@ import optimization_toolkit.cm_ts_setupdata as sensor_setup
 
 class Application:
     def __init__(self, numberOfTrials , blockSize, electrodeContacts, patientEffect, signalToNoiseRatio, currentAlgorithm, sensorModel):
-        self.disV = None
-        self.conV = None
-        self.bookV = None
-        self.vL  = None
-        self.NTrials  = numberOfTrials
-        self.divisor = blockSize
-        self.model = electrodeContacts
-        self.test = patientEffect
+        self.distributionState = None
+        self.convergenceState = None
+        self.optimizationState = None
+        self.configurationStates  = None
+        self.numberOfTrials  = numberOfTrials
+        self.blockSize = blockSize
+        self.electrodeContacts = electrodeContacts
+        self.patientEffect = patientEffect
         self.signalToNoiseRatio = signalToNoiseRatio
-        self.dst = currentAlgorithm
-        self.SDMall = sensorModel
+        self.currentAlgorithm = currentAlgorithm
+        self.sensorModel = sensorModel
     def initialize_sensor_state(self):
         try:
-            self.disV, self.conV, self.bookV, self.vL = (
-                sensor_setup({}, 1, self.NTrials, self.divisor, self.signalToNoiseRatio, self.model, self.test, self.dst, self.SDMall))
+            self.distributionState, self.convergenceState, self.optimizationState, self.configurationStates = (
+                sensor_setup({}, 1, self.numberOfTrials, self.blockSize, self.signalToNoiseRatio, self.electrodeContacts, self.patientEffect, self.currentAlgorithm, self.sensorModel))
         except Exception as e:
             print("Error initializing sensor state:", e)
     def initial_optimization_state(self):
         try:
-            if self.bookV is None or self.vL is None:
+            if self.optimizationState is None or self.configurationStates is None:
                 raise ValueError("Initialization required before optimization")
-            self.bookV['trial'] = 1
-            self.optimizationState = recommender(self.bookV, self.vL, self.disV, self.vL['distributionTypes'])
+            self.optimizationState['trial'] = 1
+            self.optimizationState = recommender(self.optimizationState, self.configurationStates, self.distributionState, self.configurationStates['distributionTypes'])
         except Exception as e:
             print("Error initializing optimization state:", e)
 
@@ -57,21 +56,22 @@ class Application:
                 # input from the UI to this reaction time and trial type here
                 reactionTime = 1.1
                 trialType = 1  # 1 - interference , 0 - no interference
-
+                trialNumber = 1
                 # code for the optimization
-                self.bookV ['trial'] = self.bookV ['trial'] + 1
+                self.optimizationState ['trial'] = trialNumber
+
                 # Sensor output after reaction time observation
                 # optimizationState, distributionState = ts_call_observer(optimizationState, distributionState,
                                                                         # configurationState, reactionTime, trialType)
-                self.bookV ,self.disV = sensor_call(self.bookV, self.disV, self.vL, reactionTime, trialType)
+                self.optimizationState ,self.distributionState = sensor_call(self.optimizationState, self.distributionState, self.configurationStates, reactionTime, trialType)
 
                 # update after observation or the output from the participant.
                 # distributionState = ts_posteriorupdates(optimizationState, distributionState, configurationState, 1)
-                self.disV = recommender_update(self.bookV, self.disV, self.vL, 1)
+                self.distributionState = recommender_update(self.optimizationState, self.distributionState, self.configurationStates, 1)
 
                 # Recommendation is generated here
-                self.bookV = recommender(self.bookV, self.vL, self.disV, self.vL['distributionTypes'])
-                self.bookV = armcount(self.bookV)
+                self.optimizationState = recommender(self.optimizationState, self.configurationStates, self.distributionState, self.configurationStates['distributionTypes'])
+                self.optimizationState = armcount(self.optimizationState)
 
                 # output from the code to the UI from here
 
@@ -85,7 +85,7 @@ if __name__ == "__main__":
         SDMall = pickle.load(f)
 
     # communication
-    app = Application(100, 15, 8, 1, 1, 'UCB', SDMall)
+    app = Application(600, 15, 8, 1, 1, 'UCB', SDMall)
 
     # Initialize the startup configuration
     app.initialize_startup_configuration()
