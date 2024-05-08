54697226d26a18b5b73ef40137b1b2d9c4bf3ab9
Sumedh Sopan Nagrale <51673422+SumedhNagrale@users.noreply.github.com>
2024-05-08 14:38:40 -0500

    Delete dummyelectron directory
    removing the dummy electron to refactor the code

--- dummyelectron/preload.js
+++
@@ -1,8 +0,0 @@
-// preload.js
-
-const { contextBridge, ipcRenderer } = require('electron');
-
-//window.ipcRenderer = ipcRenderer;
-contextBridge.exposeInMainWorld('electronAPI', {
-    setTitle: (val) => ipcRenderer.send('sendMessageToMainProcess', val)
-  })
