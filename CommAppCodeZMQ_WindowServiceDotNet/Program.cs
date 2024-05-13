c5bc6a1568e9c8c3e650eff79a4fdffc2f28ade7
Sumedh Sopan Nagrale <51673422+SumedhNagrale@users.noreply.github.com>
2024-05-13 16:05:27 -0500

    ZeroMQ dotNet
    Connection using zeroMQ

---
+++ CommAppCodeZMQ_WindowServiceDotNet/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZMQservice
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new MyService()
+                // new SavetextService // for testing with text file
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
